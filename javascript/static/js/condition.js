56b89981c9a8e22d6143ff1e3b744e9b44332aed
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-08 04:02:00 -0800

    more ternary operetr

--- javascript/static/js/condition.js
+++ javascript/static/js/condition.js
@@ -1,8 +1,14 @@
-// ternary operator pactics*******************************
-// যদি কন্ডিশন সত্য হয় ? তাইলে ভ্যালু এটা : নাইলে ভ্যালু এইটা
-const a=100;
-const b=50;
+// // ternary operator pactics*******************************
+// // যদি কন্ডিশন সত্য হয় ? তাইলে ভ্যালু এটা : নাইলে ভ্যালু এইটা
+// const a=100;
+// const b=50;
 
-const c=a>b? true : false
-console.log(c);
-document.getElementById('test').innerHTML=(c)
+// const c=a>b? true : false
+// console.log(c);
+// document.getElementById('test').innerHTML=(c)
+
+const isname= false;
+
+const s= isname ? 'yes' : 'no'
+console.log(s);
+document.getElementById('test').innerHTML=(s)
