6a1c3f0d353f36d975eb487fea5a246d3b173469
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-08 03:58:37 -0800

    pactics of ternary operetor

---
+++ javascript/static/js/condition.js
@@ -0,0 +1,8 @@
+// ternary operator pactics*******************************
+// যদি কন্ডিশন সত্য হয় ? তাইলে ভ্যালু এটা : নাইলে ভ্যালু এইটা
+const a=100;
+const b=50;
+
+const c=a>b? true : false
+console.log(c);
+document.getElementById('test').innerHTML=(c)
