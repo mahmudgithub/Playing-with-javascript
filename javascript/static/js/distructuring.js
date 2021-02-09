4c4546c4211d73e18fcb8bd67b46ea68681ee5bb
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-09 03:18:51 -0800

    first example of array distructuring

--- javascript/static/js/distructuring.js
+++ javascript/static/js/distructuring.js
@@ -1,1 +1,7 @@
-document.getElementById('test').innerHTML='hello'
+// today 9.2.2021
+
+// Array distructuring example
+// const arrName=['mahmud','hossain','lamyaa','nowsin']
+// const [first, second, love, like]=arrName
+// document.getElementById('test').innerHTML=(love)
+
