945f355ba78da377ac2d3ef525f5172d22490c9b
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-17 18:51:37 -0800

    more ternary example

--- javascript/static/js/ternaryOparetor.js
+++ javascript/static/js/ternaryOparetor.js
@@ -72,3 +72,75 @@
 //     job: 'Student'
 //  }
 //  obj.name ? obj.age ? console.log(' name and age') : obj.job ? console.log('job') : console.log('not job') : console.log('I does not');
+
+
+
+//more example of ternary operetors
+// // ternary operator pactics*******************************
+// // যদি কন্ডিশন সত্য হয় ? তাইলে ভ্যালু এটা : নাইলে ভ্যালু এইটা
+// condition ? exprIfTrue : exprIfFalse
+// const a=100;
+// const b=50;
+
+// const c=a>b? true : false
+// console.log(c);
+// document.getElementById('test').innerHTML=(c)
+
+// const isname= true;
+
+// const s= isname ? 'yes' : 'no'
+// console.log(s);
+// document.getElementById('test').innerHTML=(s)
+
+// let x=10;
+// let y=20;
+// if (x==y){console.log('false');}
+// else{console.log('true');}
+
+// let x='mahmud';
+// let y='mahmud';
+// let z=(x==y) ? 'is equal' : 'not equal'
+// console.log(z);
+
+// javascripts conditonal 
+// if,else,if-else**************************************
+// let x=100;
+// if(5<100){console.log(('x is bigger then 5'));}
+// else if(5>100){console.log(false);}
+// else{console.log('both are equal');}
+
+// using condition make voter id permission calculator
+
+// let votage=18;
+// if(votage<18){document.getElementById('test').innerHTML='no allow to vote '}
+// else if(votage>=18){document.getElementById('test').innerHTML='allow to vote'}
+// else{document.getElementById('test').innerHTML='missmatch id'}
+
+// let lol=()=>{
+//     let x=10;
+//     let y=20;
+//     if (x<y){console.log('y is bigger then x');}
+//     else if (x>y){console.log('x is bigger then y');}
+//     else{console.log('both are equal')}
+// }
+// lol()
+
+
+// today 9.2.2021 pactics 
+
+// const x=1100;
+// if (x<100){document.getElementById('test').innerHTML='right '}
+// else{document.getElementById('test').innerHTML=' wrong'}
+
+
+// const x='mahmud';
+// const y='Mahmud';
+
+// const result=(x==y) ? true : false 
+// document.getElementById('test').innerHTML=(result)
+
+// // short circuit operetors **********************
+// const a=100;
+// const b=1;
+// const result=(a==b) && false
+// document.getElementById('test').innerHTML=(result)
