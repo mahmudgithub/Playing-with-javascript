0679f905e3f468a5a43015cf1c865036b1b13ed5
mahmudgithub <mahmudhossain838@gmail.com>
2021-02-17 18:26:34 -0800

    complated asynchonus function

---
+++ javascript/static/js/setTimefunction.js
@@ -0,0 +1,12 @@
+// The setTimeout() method calls a function or evaluates an expression after
+//  a specified number of milliseconds.
+// Tip: 1000 ms = 1 second.
+// Tip: The function is only executed once. If you need to repeat execution, 
+// use the setInterval() method.
+// Tip: Use the clearTimeout() method to prevent the function from running.
+
+//Syntax****************
+// setTimeout(function, milliseconds, param1, param2, ...)
+// function lol() {
+//     setTimeout(function(){  console.log('hello'); }, 3000);}
+// lol()
