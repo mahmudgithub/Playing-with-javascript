76a23175312e0a3f4aa800c8117c1681a3039716
mahmudgithub <mahmudhossain838@gmail.com>
2021-04-12 09:37:03 +0100

    end

--- javascript/static/js/arrayBasic.js
+++ javascript/static/js/arrayBasic.js
@@ -255,3 +255,76 @@
 // const num2=num1.lastIndexOf('hossain')
 // document.getElementById('test').innerHTML=num2
 // console.log(num2);
+
+
+///pactics all method at a serial
+//make an arry
+// const arr=[1,2,3,4,5,6,7,'nowsin']
+// //access arry 
+// console.log(arr);
+// //access array by index number
+// console.log(arr[3]);
+// //change array index value
+// arr[3]='mahmud'
+// console.log(arr[3]);
+// //add new array value by index number
+// arr[7]='cool'
+// console.log(arr[7]);
+// console.log(arr);
+// //determine array length
+// console.log(arr.length);
+// //apply for loop in array 
+// for (let i=0; i<arr.length; i++)
+// {
+//     console.log(i);
+// }
+// //use puhs method in arry 
+// arr.push('lamyaa')
+// console.log(arr);
+// //pop method on arry 
+// arr.pop()
+// console.log(arr);
+// //arry unshift method
+// arr.unshift('one')
+// console.log(arr);
+// //arry shift method
+// arr.shift()
+// console.log(arr);
+// //index of methoc
+// console.log(arr.indexOf('nowsin'));
+// //splice methoc
+// arr.splice(1,3)
+// console.log(arr);
+// //join method
+// const arr2=['a','b','c']
+// console.log(arr.join(','+arr2));
+// //copy arry 
+// let arr3=arr
+// console.log(arr3);
+// //to string method
+// arr.toString()
+// console.log(arr);
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.toString(','));
+// //difference btween toString and join method
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.join('+'));  //style as wise 
+// console.log(fruits.toString(','));  //commonly use comma
+// //concrete method 
+// console.log(arr.concat(fruits));
+// //sort method
+// console.log(arr.sort());
+// //all method in one statement
+// console.log(arr.concat(fruits).sort().join('++'));
+// //reverse method 
+// console.log(arr.reverse());
+// //numerical arry value short 
+// console.log(arr.sort());
+// //decending method
+// console.log(arr.sort(function(a,b){return b-a}));
+// //difference btween revense and decending method
+// console.log(arr);
+// console.log(arr.reverse());
+// console.log(arr.sort(function(a,b){return b-a}));
+// //last indexOf method
+// console.log(arr.lastIndexOf(5));
